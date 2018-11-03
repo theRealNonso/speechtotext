7583612194298cf6165273e6fd0598d04d729291
nonso <nonsookoroafor@gmail.com>
2018-11-03 05:13:00 +0100

    changed default auth method

--- speechtotext/settings.py
+++ speechtotext/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
